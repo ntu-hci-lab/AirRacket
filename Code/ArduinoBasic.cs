1ef11753c25f74aa5a0845f234c6c8385b7939a5
Njceties <jonjoncom58@gmail.com>
2022-01-07 01:43:25 +0800

    CSharp code for arduino control in Unity added

---
+++ Code/ArduinoBasic.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using UnityEngine;
+
+public class ArduinoBasic : MonoBehaviour {
+    private SerialPort arduinoStream;
+    private SerialPort arduinoVib;
+    public string port;
+    public string portVib;
+    private Thread readThread; // 宣告執行緒
+    private Thread readVibThread;
+    public string readMessage;
+    bool isNewMessage;
+
+    void Start () {
+        if (port != "") {
+            arduinoStream = new SerialPort (port, 115200); //指定連接埠、鮑率並實例化SerialPort
+            arduinoStream.ReadTimeout = 10;
+            try {
+                arduinoStream.Open (); //開啟SerialPort連線
+                readThread = new Thread (new ThreadStart (ArduinoRead)); //實例化執行緒與指派呼叫函式
+                readThread.Start (); //開啟執行緒
+                Debug.Log ("SerialPort開啟連接");
+            } catch (System.Exception e){
+                Debug.Log ("SerialPort連接失敗");
+                Debug.Log(e.Message);
+            }
+        }
+        if (portVib != "")
+        {
+            arduinoVib = new SerialPort(portVib, 115200);
+            arduinoVib.ReadTimeout = 10;
+            try
+            {
+                arduinoVib.Open();
+                //readVibThread = new Thread(new ThreadStart(ArduinoReadVib));
+                //readVibThread.Start();
+                Debug.Log("SerialPortVib Open");
+            }
+            catch(System.Exception e)
+            {
+                Debug.Log("SerialPort Vib Fail To Open");
+                Debug.Log(e);
+            }
+        }
+    }
+    void Update () {
+        if (isNewMessage) {
+            Debug.Log (readMessage);
+        }
+        isNewMessage = false;
+    }
+    private void ArduinoRead () {
+        while (arduinoStream.IsOpen) {
+            try {
+                
+                readMessage = arduinoStream.ReadLine(); // 讀取SerialPort資料並裝入readMessage
+                isNewMessage = true;
+             
+            } catch (System.Exception e) {
+                Debug.LogWarning (e.Message);
+            }
+        }
+    }
+    private void ArduinoReadVib()
+    {
+        while (arduinoVib.IsOpen)
+        {
+            try
+            {
+
+                readMessage = arduinoVib.ReadLine(); // 讀取SerialPort資料並裝入readMessage
+                isNewMessage = true;
+
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+    public void ArduinoWrite (string message) {
+        Debug.Log (message);
+      
+        if (arduinoStream.IsOpen)
+        {
+            try
+            {
+                arduinoStream.Write(message);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+
+    public void ArduinoWriteVib (string message)
+    {
+        if (arduinoVib.IsOpen)
+        {
+            try
+            {
+                arduinoVib.Write(message);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+    void OnApplicationQuit () {
+        if (arduinoStream != null) {
+            if (arduinoStream.IsOpen) {
+                arduinoStream.Close ();
+            }
+        }
+        if (arduinoVib != null)
+        {
+            if (arduinoVib.IsOpen)
+            {
+                arduinoVib.Close();
+            }
+        }
+    }
+
+}
---
+++ Code/ArduinoControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArduinoControl : MonoBehaviour
+{	
+    public int duration = 0;
+    public int force = 0;
+    public int vibration_intensity;
+    public int vibration_duration;
+
+    private ArduinoBasic arduinoBasic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arduinoBasic = this.GetComponent<ArduinoBasic>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {	
+     	if(Input.GetKeyDown(KeyCode.Space)){
+     		Debug.Log("space is pressed");
+     		Fire();
+     	}
+    }
+
+    public void SetDuration(int duration){
+        if (duration < 0 || duration > 4000)
+            return;
+    	this.duration = duration;
+    }
+
+    public void SetForce(int force){
+        if (force < 0 || force > 255)
+            return;
+    	this.force = force;
+    }
+
+
+    public int GetDuration(){
+        return duration;
+    }
+
+    public int GetForce(){
+        return force;
+    }
+
+    public void SendConfig(){
+
+        arduinoBasic.ArduinoWrite(string.Format("c{0:d3}{1:d4}", force, duration));
+    }
+
+    public void ForceFeedback(){
+        arduinoBasic.ArduinoWrite("f");
+    }
+	public void VibeFeedback(){
+        arduinoBasic.ArduinoWriteVib(string.Format("{0:d3} {0:d3} ", vibration_duration, vibration_intensity));
+	}
+}
